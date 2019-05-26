2d8bc8c877347baca58a962fa956eaabc5e8dad8
holmosaint <32117229+holmosaint@users.noreply.github.com>
2019-05-26 23:53:14 +0800

    rewrite code

---
+++ data_generator.py
@@ -0,0 +1,215 @@
+import os
+import cv2
+import numpy as np
+import imgaug as ia
+import imgaug.augmenters as iaa
+import matplotlib.pyplot as plt
+
+crop_width = 160 
+crop_height = 160
+scale_up = iaa.Sequential([iaa.Affine(scale={"x": 1.5, "y": 2.0})])
+sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+aug_flip_horizontal = iaa.Sequential([iaa.Fliplr(0.5), iaa.CropToFixedSize(crop_width, crop_height)]) # horizontally flip 50% of all images
+aug_flip_vertical = iaa.Sequential([iaa.Flipud(0.2), iaa.CropToFixedSize(crop_width, crop_height)]) # vertically flip 20% of all images
+aug_crop = iaa.Sequential([iaa.CropToFixedSize(crop_width, crop_height)])    # crop images by -5% to 10% of their height/width
+aug_affine = iaa.Sequential([        
+    sometimes(iaa.Affine(
+        scale={"x": (0.9, 1.1), "y": (0.9, 1.1)}, # scale images to 80-120% of their size, individually per axis
+        translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)}, # translate by -20 to +20 percent (per axis)
+        rotate=(-45, 45), # rotate by -45 to +45 degrees
+        shear=(-16, 16), # shear by -16 to +16 degrees
+        order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
+        cval=(0, 255), # if mode is constant, use a cval between 0 and 255
+        mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+        )), 
+    iaa.CropToFixedSize(crop_width, crop_height)])
+aug_invert = iaa.Sequential([iaa.Invert(0.8, per_channel=True), iaa.CropToFixedSize(crop_width, crop_height)]) # invert
+
+# aug_list = [aug_flip_horizontal, aug_flip_vertical, aug_crop, aug_affine, aug_invert]
+aug_list = [aug_flip_horizontal, aug_flip_vertical, aug_crop, aug_invert]
+
+seq = iaa.Sequential(
+    [
+        # apply the following augmenters to most images
+        iaa.Fliplr(0.5), # horizontally flip 50% of all images
+        iaa.Flipud(0.2), # vertically flip 20% of all images
+        # crop images by -5% to 10% of their height/width
+        sometimes(iaa.CropAndPad(
+            percent=(-0.05, 0.1),
+            pad_mode=ia.ALL,
+            pad_cval=(0, 255)
+        )),
+        sometimes(iaa.Affine(
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
+            rotate=(-45, 45), # rotate by -45 to +45 degrees
+            shear=(-16, 16), # shear by -16 to +16 degrees
+            order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
+            cval=(0, 255), # if mode is constant, use a cval between 0 and 255
+            mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+        )),
+        # execute 0 to 5 of the following (less important) augmenters per image
+        # don't execute all of them, as that would often be way too strong
+        iaa.SomeOf((0, 5),
+            [
+                sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
+                iaa.OneOf([
+                    iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
+                    iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
+                    iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+                ]),
+                iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
+                iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
+                # search either for all edges or for directed edges,
+                # blend the result with the original image using a blobby mask
+                iaa.SimplexNoiseAlpha(iaa.OneOf([
+                    iaa.EdgeDetect(alpha=(0.5, 1.0)),
+                    iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
+                ])),
+                iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
+                iaa.OneOf([
+                    iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
+                    iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                ]),
+                iaa.Invert(0.05, per_channel=True), # invert color channels
+                iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
+                iaa.AddToHueAndSaturation((-20, 20)), # change hue and saturation
+                # either change the brightness of the whole image (sometimes
+                # per channel) or change the brightness of subareas
+                iaa.OneOf([
+                    iaa.Multiply((0.5, 1.5), per_channel=0.5),
+                    iaa.FrequencyNoiseAlpha(
+                        exponent=(-4, 0),
+                        first=iaa.Multiply((0.5, 1.5), per_channel=True),
+                        second=iaa.ContrastNormalization((0.5, 2.0))
+                    )
+                ]),
+                iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5), # improve or worsen the contrast
+                iaa.Grayscale(alpha=(0.0, 1.0)),
+                sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
+                sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
+                sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
+            ],
+            random_order=True
+        )
+    ],
+    random_order=True
+)
+
+
+def Augmentation(image_list, mask_list):
+    image_aug_list = list()
+    mask_aug_list = list()
+    # image_aug_list.extend(image_list)
+    # mask_aug_list.extend(mask_list)
+    # image_list ,mask_list = scale_up(images=image_list, heatmaps=mask_list)
+    # for image in image_list:
+    #     if image.shape[0] < crop_width or image.shape[1] < crop_height:
+    #         print("Image shape: ", image.shape)
+    for aug_method in aug_list:
+        aug_image, aug_mask = aug_method(images=image_list, heatmaps=mask_list)
+        # print("Aug image type: ", type(aug_image))
+        # print("Aug image shape: ", aug_image[0].shape)
+        # for image in aug_image:
+        #     if image.shape != (crop_width, crop_height, 3):
+        #         print("Wrong shape: ", image.shape)
+        #         print("Wrong mathod: ", aug_method)
+                #break 
+        image_aug_list.extend(aug_image)
+        mask_aug_list.extend(aug_mask)
+    
+    print("Total image after augmentation: {}".format(len(image_aug_list)))
+    print("Aug shape: ", image_aug_list[0].shape)
+    return np.array(image_aug_list).reshape(-1, crop_width, crop_height, 3), np.array(mask_aug_list).reshape(-1, crop_width, crop_height, 1)
+
+
+def DataGenerator(data_dir, gt_dir,augmentation=True):
+    """The image in the data dir should be named as ID.jpg"""
+    image_name_list = list()
+    for _, _, image_name in os.walk(data_dir):
+        image_name_list.extend(image_name)
+    
+    print("Image dir: ", data_dir)
+    image_name_list = [x for x in image_name_list if x.endswith(".jpg")]
+    image_name_list.sort()
+
+    image_list = list()
+    for image_name in image_name_list:
+        image_path = os.path.join(data_dir, image_name)
+        image = cv2.imread(image_path)
+        """if image.shape[0] < 256 or image.shape[1] < 256:
+           print("Small Image: ", image_name, image.shape)"""
+        image_list.append(image)
+    
+    """The mask i the gt dir should be named as ID_gt.bmp"""
+    print("Mask dir: ", gt_dir)
+    mask_name_list = list()
+    for _, _, mask_name in os.walk(gt_dir):
+        mask_name_list.extend(mask_name)
+
+    mask_name_list = [x for x in mask_name_list if x.endswith(".bmp")]
+    mask_name_list.sort()
+
+    mask_list = list()
+    for mask_name in mask_name_list:
+        mask_path = os.path.join(gt_dir, mask_name)
+        mask = cv2.imread(mask_path)
+        mask = mask[:, :, 0].astype(np.float32)
+        mask[mask >= 1.0] = 1.0
+        mask[mask < 0] = 0
+        mask = np.expand_dims(mask, axis=-1)
+        mask_list.append(mask)
+    
+    if not augmentation:
+        return image_list, mask_list
+    
+    return Augmentation(image_list, mask_list)
+
+
+if __name__ == "__main__":
+    image = cv2.imread("./30001.jpg")
+    mask = cv2.imread("./30001_gt.bmp")
+    image_aug_flip_horizontal, mask_aug_flip_horizontal = aug_flip_horizontal(images=[image], heatmaps=[mask.astype(np.float32)])
+    image_aug_flip_vertical, mask_aug_flip_vertical = aug_flip_vertical(images=[image], heatmaps=[mask.astype(np.float32)])
+    image_aug_crop, mask_aug_crop = aug_crop(images=[image], heatmaps=[mask.astype(np.float32)])
+    image_aug_affine, mask_aug_affine = aug_affine(images=[image], heatmaps=[mask.astype(np.float32)])
+    image_aug_invert, mask_aug_invert = aug_invert(images=[image], heatmaps=[mask.astype(np.float32)])
+    
+    plt.xticks([])
+    plt.subplot(2, 6, 1)
+    plt.imshow(image)
+    plt.axis('off')
+    plt.subplot(2, 6, 2)
+    plt.imshow(image_aug_flip_horizontal[0])
+    plt.axis('off')
+    plt.subplot(2, 6, 3)
+    plt.imshow(image_aug_flip_vertical[0])
+    plt.axis('off')
+    plt.subplot(2, 6, 4)
+    plt.imshow(image_aug_crop[0])
+    plt.axis('off')
+    plt.subplot(2, 6, 5)
+    plt.imshow(image_aug_affine[0])
+    plt.axis('off')
+    plt.subplot(2, 6, 6)
+    plt.imshow(image_aug_invert[0])    
+    plt.axis('off')
+    plt.subplot(2, 6, 7)
+    plt.imshow(mask)
+    plt.axis('off')
+    plt.subplot(2, 6, 8)
+    plt.imshow(mask_aug_flip_horizontal[0])
+    plt.axis('off')
+    plt.subplot(2, 6, 9)
+    plt.imshow(mask_aug_flip_vertical[0])
+    plt.axis('off')
+    plt.subplot(2, 6, 10)
+    plt.imshow(mask_aug_crop[0])
+    plt.axis('off')
+    plt.subplot(2, 6, 11)
+    plt.imshow(mask_aug_affine[0])
+    plt.axis('off')
+    plt.subplot(2, 6, 12)
+    plt.imshow(mask_aug_invert[0])
+    plt.axis('off')
+    plt.savefig("./aug_com.jpg", dpi=200)
---
+++ main.py
@@ -0,0 +1,322 @@
+import argparse
+import time
+import numpy as np
+from network import Encoder
+from data_generator import DataGenerator
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn.utils import shuffle
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+parser = argparse.ArgumentParser(description="Foreground Segmentation for 2019 PKU Image Processing Course")
+parser.add_argument("--gpu", default=[False], help="Whether to use gpu", nargs=1, type=bool)
+parser.add_argument("--encoder", default=["resnet"], help="Choose the architecture of the encoder network: vgg, resnet or segnet", nargs=1)
+parser.add_argument("--train_image_dir", default=["./orderedImages/train"], nargs=1, type=str)
+parser.add_argument("--train_mask_dir", default=["./orderedTruths/train"], nargs=1, type=str)
+parser.add_argument("--val_image_dir", default=["./orderedImages/val"], nargs=1, type=str)
+parser.add_argument("--val_mask_dir", default=["./orderedTruths/val"], nargs=1, type=str)
+parser.add_argument("--test_image_dir", default=["./orderedImages/test"], nargs=1, type=str)
+parser.add_argument("--test_mask_dir", default=["./orderedTruths/test"], nargs=1, type=str)
+parser.add_argument("--train", default=[False], nargs=1, type=bool)
+parser.add_argument("--test", default=[False], nargs=1, type=bool)
+parser.add_argument("--epoch", default=[int(1e3)], nargs=1, type=int)
+parser.add_argument("--step_per_epoch", default=[100], nargs=1, type=int)
+parser.add_argument("--batch", default=[32], nargs=1, type=int)
+parser.add_argument("--aug", default=[None], nargs=1, type=str)
+
+edition = str(time.asctime(time.localtime(time.time())))
+
+def adjust_learning_rate(optimizer):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    for param_group in optimizer.param_groups:
+        param_group['lr'] /= 5
+
+
+def train_aug(train_image_list, train_mask_list, val_image_list, val_mask_list, epoch, step_per_epoch, batch, net, device, encoder):
+    lst_train_loss = 1e8
+    lst_val_loss = 1e8
+    not_improve = 0
+    train_sample_num = train_image_list.shape[0]
+    val_sample_num = val_image_list.shape[0]
+    # print(train_image_list.shape)
+    adam = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=1e-3, weight_decay=1e-3, amsgrad=True)
+    print("Training started!")
+    for t in range(epoch):
+        print("Epoch {}".format(t))
+        train_image_list, train_mask_list = shuffle(train_image_list, train_mask_list, random_state=0)
+        train_loss = 0.0
+        val_loss = 0.0
+        """Train"""
+        for s in range(0, train_sample_num, batch):
+            sample = np.arange(s, min(s + batch, train_sample_num)).astype(np.int)
+            images = train_image_list[sample, :]
+            masks = train_mask_list[sample, :]
+            if encoder == "segnet":
+                inv_masks = 1 - masks
+                masks = np.concatenate((inv_masks, masks), axis=-1)
+            images = torch.FloatTensor(images).to(device).unsqueeze(0).permute(0, 3, 1, 2) / 255.0
+            masks = torch.FloatTensor(masks).to(device).unsqueeze(0).permute(0, 3, 1, 2)
+            pre_mask = net(images)
+            loss = F.binary_cross_entropy(pre_mask, masks)
+            loss = loss.mean(0)
+            train_loss += loss.item()
+
+            # update
+            adam.zero_grad()
+            loss.backward()
+            adam.step()
+
+        """Validation"""
+        for s in range(0, val_sample_num, batch):
+            sample = np.arange(s, min(s + batch, val_sample_num)).astype(np.int)
+            images = val_image_list[sample, :]
+            masks = val_mask_list[sample, :]
+            images = val_image_list[s]
+            masks = val_mask_list[s]
+            
+            if encoder == "segnet":
+                inv_masks = 1 - masks
+                masks = np.concatenate((inv_masks, masks), axis=-1)
+
+            images = torch.FloatTensor(images).to(device).unsqueeze(0).permute(0, 3, 1, 2) / 255.0
+            masks = torch.FloatTensor(masks).to(device).unsqueeze(0).permute(0, 3, 1, 2)
+            pre_mask = net(images).detach()
+            loss = F.binary_cross_entropy(pre_mask, masks)
+            loss = loss.mean(0)
+            val_loss += loss.item()
+
+        if train_loss < lst_train_loss and val_loss < lst_val_loss:
+            not_improve = 0
+            lst_train_loss = train_loss
+            lst_val_loss = val_loss
+            torch.save(net.state_dict(), "./model/FgSegNet"+edition)
+        else:
+            not_improve += 1
+            if not_improve == 100:
+                print("Early Stop at epoch {}".format(t))
+                break
+            elif not_improve % 10 == 0 and not_improve > 0:
+                adjust_learning_rate(adam)
+
+        print("\tTrain loss {}\tVal loss {}".format(train_loss / train_sample_num * batch, val_loss / val_sample_num * batch))
+
+
+def train_no_aug(train_image_list, train_mask_list, val_image_list, val_mask_list, epoch, step_per_epoch, batch, net, device, encoder):
+    lst_train_loss = 1e8
+    lst_val_loss = 1e8
+    not_improve = 0
+    train_sample_num = len(train_image_list)
+    val_sample_num = len(val_mask_list)
+    adam = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=1e-3, weight_decay=1e-3, amsgrad=True)
+    print("Training started!")
+    for t in range(epoch):
+        print("Epoch {}".format(t))
+        train_image_list, train_mask_list = shuffle(train_image_list, train_mask_list, random_state=0)
+        train_loss = 0.0
+        val_loss = 0.0
+        """Train"""
+        for s in range(0, train_sample_num, batch):
+            images = train_image_list[s]
+            masks = train_mask_list[s]
+
+            if encoder == "segnet":
+                inv_masks = 1 - masks
+                masks = np.concatenate((inv_masks, masks), axis=-1)
+
+            images = torch.FloatTensor(images).to(device).unsqueeze(0).permute(0, 3, 1, 2) / 255.0
+            masks = torch.FloatTensor(masks).to(device).unsqueeze(0).permute(0, 3, 1, 2)
+            pre_mask = net(images)
+            loss = F.binary_cross_entropy(pre_mask, masks)
+            loss = loss.mean(0)
+            train_loss += loss.item()
+
+            # update
+            adam.zero_grad()
+            loss.backward()
+            adam.step()
+
+        """Validation"""
+        for s in range(0, val_sample_num, batch):
+            images = val_image_list[s]
+            masks = val_mask_list[s]
+
+            if encoder == "segnet":
+                inv_masks = 1 - masks
+                masks = np.concatenate((inv_masks, masks), axis=-1)
+                
+            images = torch.FloatTensor(images).to(device).unsqueeze(0).permute(0, 3, 1, 2) / 255.0
+            masks = torch.FloatTensor(masks).to(device).unsqueeze(0).permute(0, 3, 1, 2)
+            pre_mask = net(images).detach()
+            loss = F.binary_cross_entropy(pre_mask, masks)
+            loss = loss.mean(0)
+            val_loss += loss.item()
+
+        if train_loss < lst_train_loss and val_loss < lst_val_loss:
+            not_improve = 0
+            lst_train_loss = train_loss
+            lst_val_loss = val_loss
+            torch.save(net.state_dict(), "./model/FgSegNet"+edition)
+        else:
+            not_improve += 1
+            if not_improve == 100:
+                print("Early Stop at epoch {}".format(t))
+                break
+            elif not_improve % 10 == 0 and not_improve > 0:
+                adjust_learning_rate(adam)
+
+        print("\tTrain loss {}\tVal loss {}".format(train_loss / train_sample_num * batch, val_loss / val_sample_num * batch))
+
+
+def train_keras_aug(train_image_list, train_mask_list, val_image_list, val_mask_list, epoch, step_per_epoch, batch, net, device, encoder):
+    print("Train image: ", train_image_list)
+    print("Train mask: ", train_mask_list)
+    print("Val image: ", val_image_list)
+    print("Val mask: ", val_mask_list)
+    train_datagen = ImageDataGenerator(rotation_range=0.2,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                shear_range=0.2,
+                zoom_range=0.2,
+                horizontal_flip=True,
+                fill_mode='nearest')
+    train_image_generator = train_datagen.flow_from_directory(train_image_list, class_mode=None, batch_size=32, seed=1)
+    train_mask_generator = train_datagen.flow_from_directory(train_mask_list, class_mode=None, batch_size=32, seed=1)
+    train_generator = zip(train_image_generator, train_mask_generator)
+
+    val_datagen = ImageDataGenerator()
+    val_image_generator = val_datagen.flow_from_directory(val_image_list, class_mode=None, batch_size=32, seed=2)
+    val_mask_generator = val_datagen.flow_from_directory(val_mask_list, class_mode=None, batch_size=32, seed=2)
+    val_generator = zip(val_image_generator, val_mask_generator)
+
+    lst_train_loss = 1e8
+    lst_val_loss = 1e8
+    not_improve = 0
+    adam = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=1e-3, weight_decay=1e-3, amsgrad=True)
+    for t in range(epoch):
+        print("Epoch {}".format(t))
+        train_loss = 0.0
+        s = 0
+        for gen in train_generator:
+            image, masks = gen
+            # print(masks.shape)
+            masks = masks[:, :, :, 0:1]
+            masks[masks>=1] = 1
+            image = torch.FloatTensor(image).permute(0, 3, 1, 2).to(device)
+
+            pre_mask = net(image)
+
+            if encoder == "segnet":
+                inv_masks = 1 - masks
+                masks = np.concatenate((inv_masks, masks), axis=-1)
+
+            masks = torch.FloatTensor(masks).to(device).permute(0, 3, 1, 2)
+            loss = F.binary_cross_entropy(pre_mask, masks)
+            loss = loss.mean(0)
+            train_loss += loss.item()
+
+            # update
+            adam.zero_grad()
+            loss.backward()
+            adam.step()
+
+        val_loss = 0
+        for i in range(12):
+            for gen in val_generator:
+                image, masks = gen
+                masks[masks>=1] = 1
+                image = torch.FloatTensor(image).permute(0, 3, 1, 2).to(device)
+
+                pre_mask = net(image)
+
+                if encoder == "segnet":
+                    inv_masks = 1 - masks
+                    masks = np.concatenate((inv_masks, masks), axis=-1)
+
+                masks = torch.FloatTensor(masks).to(device).permute(0, 3, 1, 2)
+                loss = F.binary_cross_entropy(pre_mask, masks)
+                loss = loss.mean(0)
+                val_loss += loss.item()
+                break
+
+        if train_loss < lst_train_loss and val_loss < lst_val_loss:
+            torch.save(net.state_dict(), "./model/FgSegNet"+edition)
+            lst_train_loss = train_loss
+            lst_val_loss = val_loss
+            not_improve = 0
+        else:
+            not_improve += 1
+            if not_improve == 100:
+                print("Early Stop at epoch {}".format(t))
+                break
+            elif not_improve % 10 == 0 and not_improve > 0:
+                adjust_learning_rate(optim, t)
+        print("\tTrain loss: {}; Valid loss: {}".format(train_loss / step_per_epoch, val_loss / 6))
+
+
+def train_dispatch(train_image_dir, train_mask_dir, val_image_dir, val_mask_dir, epoch, step_per_epoch, batch, net, device, aug, encoder):
+    if aug is None:        
+        train_image_list, train_mask_list = DataGenerator(data_dir=train_image_dir, gt_dir=train_mask_dir, augmentation=False)
+        val_image_list, val_mask_list = DataGenerator(data_dir=val_image_dir, gt_dir=val_mask_dir, augmentation=False)
+        train_no_aug(train_image_list, train_mask_list, val_image_list, val_mask_list, 
+            epoch=epoch, step_per_epoch=step_per_epoch, net=net, batch=batch, device=device, encoder=encoder)
+    elif aug == "keras":
+        train_keras_aug(train_image_dir, train_mask_dir, val_image_dir, val_mask_dir, epoch, step_per_epoch, batch, net, device, encoder=encoder)
+    else:
+        train_image_list, train_mask_list = DataGenerator(data_dir=train_image_dir, gt_dir=train_mask_dir, augmentation=True)
+        val_image_list, val_mask_list = DataGenerator(data_dir=val_image_dir, gt_dir=val_mask_dir, augmentation=True)
+        train_aug(train_image_list, train_mask_list, val_image_list, val_mask_list, 
+            epoch=epoch, step_per_epoch=step_per_epoch, net=net, batch=batch, device=device, encoder=encoder)
+        
+
+def test(test_image_list, test_mask_list, net, device, model_name="./model/FgSegNetSun May 26 18:08:13 2019"):
+    net.load_state_dict(torch.load(model_name))
+    test_sample_num = len(test_image_list)
+    for s in range(test_sample_num):
+        images = test_image_list[s]
+        masks = test_mask_list[s]
+        images = torch.FloatTensor(images).to(device).unsqueeze(0).permute(0, 3, 1, 2) / 255.0
+        masks = torch.FloatTensor(masks).to(device).unsqueeze(0).permute(0, 3, 1, 2)
+        pre_mask = net(images).detach()
+        pre_mask = torch.argmax(pre_mask, dim=1, keepdim=True)
+        pre_mask = pre_mask.squeeze(0).squeeze(0)
+        pre_mask = pre_mask.cpu().numpy()
+        np.savetxt("./result/" + str(30000 + s + 1) + ".txt", pre_mask, fmt="%.3f")
+        
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    args_dict = dict()
+    args_dict["cuda"] = args.gpu[0]
+    args_dict["encoder_net"] = args.encoder[0]
+    args_dict["train_image_dir"] = args.train_image_dir[0]
+    args_dict["train_mask_dir"] = args.train_mask_dir[0]
+    args_dict["val_image_dir"] = args.val_image_dir[0]
+    args_dict["val_mask_dir"] = args.val_mask_dir[0]
+    args_dict["test_image_dir"] = args.test_image_dir[0]
+    args_dict["test_mask_dir"] = args.test_mask_dir[0]
+    args_dict["is_train"] = args.train[0]
+    args_dict["is_test"] = args.test[0]
+    args_dict["epoch"] = args.epoch[0]
+    args_dict["step_per_epoch"] = args.step_per_epoch[0]
+    args_dict["batch"] = args.batch[0]
+    args_dict["aug"] = args.aug[0]
+
+    if args_dict["cuda"]:
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    encoder = Encoder(structure=args_dict["encoder_net"], cuda=args_dict["cuda"])
+    net = encoder.net.to(device)
+
+    if args_dict["is_train"]:
+        train_dispatch(train_image_dir=args_dict["train_image_dir"], train_mask_dir=args_dict["train_mask_dir"], 
+            val_image_dir=args_dict["val_image_dir"], val_mask_dir=args_dict["val_mask_dir"], 
+            epoch=args_dict["epoch"], step_per_epoch=args_dict["step_per_epoch"], net=net, batch=args_dict["batch"], 
+            device=device, encoder=args_dict["encoder_net"], aug=args_dict["aug"])
+    
+    if args_dict["is_test"]:
+        test_image_list, test_mask_list = DataGenerator(data_dir=args_dict["test_image_dir"], gt_dir=args_dict["test_mask_dir"], augmentation=False)
+        test(test_image_list, test_mask_list, net, device)
